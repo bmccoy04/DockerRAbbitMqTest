46a21b3f0a1ddc59cc53df83aaa30e1ed0cea19a
Bryan McCoy <bryanmccoy04@gmail.com>
2018-08-08 20:57:26 -0500

    Cleaned up for a demo

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+5672
+
+ docker run -it --rm -p 5672:5672 rabbitmq 
--- Sender/Program.cs
+++ Sender/Program.cs
@@ -8,10 +8,6 @@ namespace DockerRabbitMqTest
     {
         static void Main(string[] args)
         {
-            //  Console.WriteLine("Sleeping ....");
-            //  System.Threading.Thread.Sleep(25000);
-            //  Console.WriteLine("Up Now ....");
-
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
@@ -31,25 +27,3 @@ namespace DockerRabbitMqTest
     }
 }
 
-// var factory = new ConnectionFactory() { HostName = "localhost" };
-// using (var connection = factory.CreateConnection())
-// using (var channel = connection.CreateModel())
-// {
-//     channel.QueueDeclare(queue: "hello",
-//                          durable: false,
-//                          exclusive: false,
-//                          autoDelete: false,
-//                          arguments: null);
-
-//     string message = "Hello World!";
-//     var body = Encoding.UTF8.GetBytes(message);
-
-//     channel.BasicPublish(exchange: "",
-//                          routingKey: "hello",
-//                          basicProperties: null,
-//                          body: body);
-//     Console.WriteLine(" [x] Sent {0}", message);
-// }
-
-// Console.WriteLine(" Press [enter] to exit.");
-// Console.ReadLine();
